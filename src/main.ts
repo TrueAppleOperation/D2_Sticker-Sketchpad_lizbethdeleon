cc31841a34eb3506c4e34e54cdcde523c7fbf9c8
Fishbowl <icecoffee1945@gmail.com>
2025-10-16 06:29:44 +0000

    Initial non-interactive UI layout | Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+import IconUrl from "./drawing.jpg";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p><img src="${IconUrl}" class="icon" /></p>
+  <canvas id = "canvas" width = "256" height = "256"></canvas>
 `;
+
+const title = document.createElement("h1");
+title.textContent = "Sticker Sketchpad";
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+document.body.insertBefore(title, canvas);
